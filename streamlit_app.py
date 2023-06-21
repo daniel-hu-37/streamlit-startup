9e834a1c7716fdd285d2f1c352c133721c097065
Daniel Hu <dh37@princeton.edu>
2023-06-21 10:24:55 -0400

    slimmed down code a little

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,18 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-st.write('Hello, *World!* :sunglasses:')
+st.header('Hello, World! :sunglasses:')
 
-# Example 2
-
-st.write(1234)
-
-# Example 3
-
+# Dataframe 1
+st.subheader('Dataframe 1')
 df = pd.DataFrame({
      'first column': [1, 2, 3, 4],
      'second column': [10, 20, 30, 40]
      })
 st.write(df)
 
-# Example 4
-
+# Chart
 st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# Example 5
-
 df2 = pd.DataFrame(
      np.random.randn(200, 3),
      columns=['a', 'b', 'c'])
